8a74ec79a9c98fa8fc493d2ae71744f6a594dfd2
abebrege <abrege@northcrossgroup.com>
2026-08-09 13:43:57 -0400

    fix api key handling

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import argparse
+import os
 from src.processor import process
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
 
 def main():
     parser = argparse.ArgumentParser(
         description="LLM-based Cryptographic API Misuse Detection",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-
-    # folder_path = './pysafe_trapfile'     # folder path be analyzed
-    folder_path = './py_full_unsafe'  # folder path be analyzed
+    parser.add_argument(
+        "--target",
+        type=str,
+        default="./py_full_unsafe",
+        help="Path to the file or folder containing source code file(s) to be analyzed. If pointed to a directory, it will automatically batch scan the source files in the directory."
+    )
+    args = parser.parse_args()
+    target_path = args.target
 
     model_name = 'deepseek-ai/DeepSeek-V3.1-Terminus'
     # model_name = 'moonshotai/Kimi-K2-Instruct'  # model name from SiliconFlow page: https://cloud.siliconflow.cn/sft-143zof85kk/models
-
-    API_Key = 'sk-cvjxxxx'  # Please fill in your own SiliconFlow API key.
-
-    process(folder_path, 'py', model_name, API_Key, 1)
+    API_Key = os.getenv('API_Key')
+    if not API_Key:
+        raise RuntimeError("API_Key is not set. Please set it in the .env file.")
+    process(target_path, 'py', model_name, API_Key, 1)
 
 if __name__ == "__main__":
     main()
