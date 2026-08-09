844665e81e708cc975f99420359794bdc23e2a25
abebrege <abrege@northcrossgroup.com>
2026-08-09 13:25:39 -0400

    extract to util file

--- src/cot_ex.py
+++ src/cot_ex.py
@@ -1,63 +1,10 @@
-import os
-import csv
-import re
 import time
 import requests
-import rules as ru
 import argparse
 from pathlib import Path
 from tqdm import tqdm
 import random
-
-
-def find_java_files(folder_path: str) -> list:
-    java_files = []
-    for root, _, files in os.walk(folder_path):
-        for file in files:
-            if file.endswith(".java"):
-                java_files.append(Path(root) / file)
-    return java_files
-
-
-def find_py_files(folder_path: str) -> list:
-    py_files = []
-    for root, _, files in os.walk(folder_path):
-        for file in files:
-            if file.endswith(".py"):
-                py_files.append(Path(root) / file)
-    return py_files
-
-
-def load_security_rules(code_type: str = None) -> (dict, dict):
-    """Load rule_groups from rule_source.py"""
-    try:
-        if code_type == 'java':
-            return ru.rule_groups_java
-        elif code_type == 'py':
-            return ru.rule_groups
-    except ImportError:
-        raise RuntimeError("Cannot Load rule_groups!")
-
-
-def read_code_file(file_path: str) -> str:
-    try:
-        path = Path(file_path)
-        if not path.is_file():
-            raise ValueError("Path is not files")
-        if path.suffix not in ['.py', '.java']:
-            raise ValueError("Only support .py and .java files")
-
-        with open(path, 'r', encoding='utf-8') as f:
-            lines = f.readlines()
-
-        numbered_lines = [f"{i + 1}: {line}" for i, line in enumerate(lines)]
-        content = ''.join(numbered_lines)
-
-        return content
-
-    except Exception as e:
-        raise RuntimeError(f"Fail read files: {str(e)}")
-
+from util import find_java_files, find_py_files, load_security_rules, read_code_file, save_to_csv, parse_analysis_result
 
 def prepare_prompt(source_code: str, rule_groups: dict) -> str:
     rule_descriptions = []
@@ -109,39 +56,6 @@ Misused Modules: (multiple modules separated by '|'. If the detection result is 
 Misused path:
     """
     return instruction
-
-
-def parse_analysis_result(text: str, file_path: str) -> dict:
-    result = {
-        "File Path": '',
-        "Line Numbers": '',
-        "Rule IDs": '',
-        "Rule Names": '',
-        "Misused Modules": ''
-    }
-    result["File Path"] = file_path
-    patterns = [
-        r'Line Numbers[：:]\s*([^.\n]+?)\s*\n',
-        r'Rule IDs[：:]\s*([\d\s,\|\-]+)\n',
-        r'Rule Names[：:]\s*([^.\n]+?)\s*\n',
-        r'Misused Modules[：:]\s*([^.\n]+?)\s*\n'
-    ]
-
-    match1 = re.search(patterns[0], text)
-    if match1:
-        result['Line Numbers'] = match1.group(1)
-    match2 = re.search(patterns[1], text)
-    if match2:
-        result['Rule IDs'] = match2.group(1)
-    match3 = re.search(patterns[2], text)
-    if match3:
-        result['Rule Names'] = match3.group(1)
-    match4 = re.search(patterns[3], text)
-    if match4:
-        result['Misused Modules'] = match4.group(1)
-
-    return result
-
 
 def analyze_with_llm(prompt: str, model_name: str, API_Key: str, timeout: int = 120):
     """
@@ -279,17 +193,6 @@ def process_single_file(file_path: Path, rule_groups: dict, model_name: str, API
         "Misused Modules": str(e),
         "Time_Taken/s": '0.01'
     }
-
-
-def save_to_csv(results: list[dict], output_path: str):
-    """save structure result to CSV"""
-    fieldnames = ["File Path", "Line Numbers", "Rule IDs", "Rule Names", "Misused Modules", "Time_Taken/s"]
-
-    with open(output_path, "w", newline="", encoding="utf-8-sig") as f:
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(results)
-
 
 def batch_analyze(folder_path: str, codetype: str, model_name: str, API_Key: str, i):
 # def batch_analyze(folder_path: str, codetype: str, model_name: str, i):
---
+++ src/util.py
@@ -0,0 +1,93 @@
+import os
+from pathlib import Path
+import csv
+import rules as ru
+import re
+
+def find_java_files(folder_path: str) -> list:
+    java_files = []
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(".java"):
+                java_files.append(Path(root) / file)
+    return java_files
+
+
+def find_py_files(folder_path: str) -> list:
+    py_files = []
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(".py"):
+                py_files.append(Path(root) / file)
+    return py_files
+
+
+def load_security_rules(code_type: str = None) -> (dict, dict):
+    """Load rule_groups from rule_source.py"""
+    try:
+        if code_type == 'java':
+            return ru.rule_groups_java
+        elif code_type == 'py':
+            return ru.rule_groups
+    except ImportError:
+        raise RuntimeError("Cannot Load rule_groups!")
+
+
+def read_code_file(file_path: str) -> str:
+    try:
+        path = Path(file_path)
+        if not path.is_file():
+            raise ValueError("Path is not files")
+        if path.suffix not in ['.py', '.java']:
+            raise ValueError("Only support .py and .java files")
+
+        with open(path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+
+        numbered_lines = [f"{i + 1}: {line}" for i, line in enumerate(lines)]
+        content = ''.join(numbered_lines)
+
+        return content
+
+    except Exception as e:
+        raise RuntimeError(f"Fail read files: {str(e)}")
+
+def save_to_csv(results: list[dict], output_path: str):
+    """save structure result to CSV"""
+    fieldnames = ["File Path", "Line Numbers", "Rule IDs", "Rule Names", "Misused Modules", "Time_Taken/s"]
+
+    with open(output_path, "w", newline="", encoding="utf-8-sig") as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(results)
+
+def parse_analysis_result(text: str, file_path: str) -> dict:
+    result = {
+        "File Path": '',
+        "Line Numbers": '',
+        "Rule IDs": '',
+        "Rule Names": '',
+        "Misused Modules": ''
+    }
+    result["File Path"] = file_path
+    patterns = [
+        r'Line Numbers[：:]\s*([^.\n]+?)\s*\n',
+        r'Rule IDs[：:]\s*([\d\s,\|\-]+)\n',
+        r'Rule Names[：:]\s*([^.\n]+?)\s*\n',
+        r'Misused Modules[：:]\s*([^.\n]+?)\s*\n'
+    ]
+
+    match1 = re.search(patterns[0], text)
+    if match1:
+        result['Line Numbers'] = match1.group(1)
+    match2 = re.search(patterns[1], text)
+    if match2:
+        result['Rule IDs'] = match2.group(1)
+    match3 = re.search(patterns[2], text)
+    if match3:
+        result['Rule Names'] = match3.group(1)
+    match4 = re.search(patterns[3], text)
+    if match4:
+        result['Misused Modules'] = match4.group(1)
+
+    return result
